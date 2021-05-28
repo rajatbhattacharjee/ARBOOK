14b338fca7cc8443ee15ae9ac65022182e293ca0
Your Name <you@example.com>
2021-05-27 20:10:02 -0500

    first commit

---
+++ Assets/Scripts/EndPlaneScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPlaneScript : MonoBehaviour {
+
+	// Use this for initialization
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Boat_1800early_BrigSloop_LOD_0")
+        {
+            Debug.Log("BATMAN!");
+            //print("No longer in contact with " + gameObject.transform.name);
+            //other.gameObject.transform.position = new Vector3(0, 0, 0);
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ReachingEndBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReachingEndBehaviourScript : MonoBehaviour {
+
+    // Use this for initialization
+    Vector3 orginalPosition;
+    void Start () {
+        orginalPosition = transform.localPosition;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "END")
+        {
+            transform.localPosition = orginalPosition;
+        }
+        else if (other.gameObject.tag == "AWALL")
+        {
+            transform.localPosition = orginalPosition;
+        }
+        else if (other.gameObject.tag == "CWALL")
+        {
+            transform.localPosition = orginalPosition;
+        }
+    }
+}
